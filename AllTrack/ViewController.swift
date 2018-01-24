ae407e4b93410959544759d5ac9f6707774d0742
kevin <kevinkang88@gmail.com>
2018-01-23 16:35:43 -0800

    Initial Commit

---
+++ AllTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllTrack
+//
+//  Created by kevin on 1/23/18.
+//  Copyright © 2018 yooniverse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
