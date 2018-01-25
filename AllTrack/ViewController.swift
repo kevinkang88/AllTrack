04e5538b663d20d5990ea9895e71189e77f0618a
kevin <kevinkang88@gmail.com>
2018-01-24 19:51:31 -0800

    set up ARKit. user is able to interact with SCNView and add SCNCapsule in random position around World Origin

--- AllTrack/ViewController.swift
+++ AllTrack/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var sceneView: ARSCNView!
+    let config = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(config)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let x = randomNumbers(numOne: -0.3, numTwo: 0.3)
+        let y = randomNumbers(numOne: -0.3, numTwo: 0.3)
+        let z = randomNumbers(numOne: -0.3, numTwo: 0.3)
+        node.position = SCNVector3(x,y,z)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        pauseSession()
+    }
+    
+    func pauseSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func randomNumbers(numOne:CGFloat, numTwo:CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(numOne - numTwo) + min(numOne, numTwo)
+    }
 }
 
