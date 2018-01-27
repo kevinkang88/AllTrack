51b0c94289a94cccf8d83ca9125eb0af28336656
kevin <kevinkang88@gmail.com>
2018-01-26 20:44:51 -0800

    create snowman

--- AllTrack/ViewController.swift
+++ AllTrack/ViewController.swift
@@ -27,13 +27,35 @@ class ViewController: UIViewController {
 
     @IBAction func addButtonTapped(_ sender: Any) {
         let bodyNode = SCNNode()
-        bodyNode.geometry = SCNSphere(radius: 0.3)
-        bodyNode.geometry?.firstMaterial?.specular.contents = UIColor.white
-        bodyNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        let x = 0.3
-        let y = 0.3
-        let z = 0.3
+        bodyNode.geometry = SCNSphere(radius: 0.1)
+        bodyNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
+        bodyNode.geometry?.firstMaterial?.specular.contents = UIColor.lightGray
+        let x = 0.1
+        let y = 0.1
+        let z = 0.1
+        
+        let headNode = SCNNode()
+        headNode.geometry = SCNSphere(radius: 0.05)
+        headNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
+        headNode.geometry?.firstMaterial?.specular.contents = UIColor.lightGray
+        headNode.position = SCNVector3(0,0.15,0)
+        
+        let leftEye = SCNNode()
+        leftEye.geometry = SCNPlane(width: 0.01, height: 0.02)
+        leftEye.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        leftEye.geometry?.firstMaterial?.specular.contents = UIColor.lightGray
+        leftEye.position = SCNVector3(-0.01,0,0.05)
+        
+        let rightEye = SCNNode()
+        rightEye.geometry = SCNPlane(width: 0.01, height: 0.02)
+        rightEye.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        rightEye.geometry?.firstMaterial?.specular.contents = UIColor.lightGray
+        rightEye.position = SCNVector3(0.01,0,0.05)
+        
         bodyNode.position = SCNVector3(x,y,z)
+        headNode.addChildNode(rightEye)
+        headNode.addChildNode(leftEye)
+        bodyNode.addChildNode(headNode)
         self.sceneView.scene.rootNode.addChildNode(bodyNode)
     }
     
